449739ba70f50f4a735369e27c0a1d1b071454f7
szczesnym <marcins@oerlemans-foods.nl>
2018-08-19 13:36:15 +0200

    BoradsULName?-remove update

--- script.js
+++ script.js
@@ -112,7 +112,7 @@ $(document).ready(function() {
     var requestUrl = apiRoot + 'tasks';
 
     $.ajax({
-      url: requestUrl + '/?' + taskId,
+      url: requestUrl + '/' + taskId,
       method: 'DELETE',
       success: function() {
         parentEl.slideUp(400, function() { parentEl.remove(); });
